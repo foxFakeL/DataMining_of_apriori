f22dbd6457d99243bbfb1cc32e1030ff75ac0683
foxFakeL <3196718529@qq.com>
2024-01-13 23:05:21 +0800

    fix CMakeLists.txt

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/l/Code/DataMining_of_apriori
-# Build directory: /home/l/Code/DataMining_of_apriori/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
